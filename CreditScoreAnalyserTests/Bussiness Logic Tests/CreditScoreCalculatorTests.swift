f08f1a9f88d5e7ee9ea5915562f36d5904053514
Vinay Hosamane K N <vinay.k.n.hosamane@sap.com>
2021-03-28 12:33:11 +0530

    updates test cases for business logic.

--- CreditScoreAnalyserTests/Bussiness Logic Tests/CreditScoreCalculatorTests.swift
+++ CreditScoreAnalyserTests/Bussiness Logic Tests/CreditScoreCalculatorTests.swift
@@ -4,29 +4,68 @@
 //
 //  Created by Hosamane, Vinay K N on 28/03/21.
 //
-
+@testable import CreditScoreAnalyser
 import XCTest
 
+/*
+ 1. use new mock json file
+ 2. after deserialisation, validate the decoded model and it's values.
+ 3. test getCreditScore() method
+ 4. test getCreditScoreScales() method
+ 5. test getCreditScoreColor() method
+ 6. test doesScoreBelongsToScale() method
+ */
+
 class CreditScoreCalculatorTests: XCTestCase {
+    
+    var creditScoreCalculator: CreditScoreCalculatable? = nil
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // load mock json to the credit score calculator
+        creditScoreCalculator = CreditScoreCalculator(with: "MockCreditScoreReport")
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        creditScoreCalculator = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testGetCreditScore() {
+        guard let report = creditScoreCalculator else {
+            XCTFail("Could not find valid score calculator")
+            return
         }
+        XCTAssert(report.getCreditScore() == 811.0, "Validate if the credit score form json is same as inside the report.")
+    }
+    
+    func testGetCreditScoreScales() {
+        guard let report = creditScoreCalculator else {
+            XCTFail("Could not find valid score calculator")
+            return
+        }
+        XCTAssert(report.getCreditScoreScales()?.count == 5, "Check if we have got 5 scales from json.")
+    }
+    
+    func testGetCreditScoreColor() {
+        guard let report = creditScoreCalculator else {
+            XCTFail("Could not find valid score calculator")
+            return
+        }
+        //(hex)#D8AC3D --- rgb(216,172,61)
+        XCTAssert(report.getCreditScoreColor() == UIColor(hexRGB: "#D8AC3D"), "Validate the color for credit score.")
+    }
+    
+    func testDoesScoreBelongsToScale() {
+        guard let report = creditScoreCalculator else {
+            XCTFail("Could not find valid score calculator")
+            return
+        }
+        let sampleScale = Scale(type: "highest",
+                                min: 825,
+                                max: 900,
+                                colorHex: "#A3C65D")
+        let score = report.getCreditScore()
+        
+        XCTAssert(report.doesScoreBelongsToScale(score: score, scale: sampleScale) == false, "check if the score is available in the given scale.")
     }
 
 }
